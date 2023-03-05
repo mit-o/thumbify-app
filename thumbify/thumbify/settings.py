4151cf1645b57b30c6d3ade1372d695abff8bc2c
Sebastian Tarczyński <starczynski7@gmail.com>
2023-03-05 16:50:13 +0100

    add custom user

--- thumbify/thumbify/settings.py
+++ thumbify/thumbify/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User Model
+
+AUTH_USER_MODEL = "accounts.User"
