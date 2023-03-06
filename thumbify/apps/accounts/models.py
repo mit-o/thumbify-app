f6bd3d05f1a7e7f1f18c7cd72ebf0c883cbe155d
Sebastian Tarczyński <starczynski7@gmail.com>
2023-03-06 04:55:48 +0100

    mv UserManager to managers

--- thumbify/apps/accounts/models.py
+++ thumbify/apps/accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
 )
-from django.contrib.auth.base_user import BaseUserManager
+
+from .managers import UserManager
 
 
 class AccountTier(models.Model):
@@ -19,30 +20,6 @@ class AccountTier(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError(("The Email must be set."))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(("SuperUser must be assigned to staff role"))
-
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(("Super User must be assigned to super user role"))
-
-        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
