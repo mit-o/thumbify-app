3353dddc6c9611ff3fe6efc67e77356ae570c820
Sebastian Tarczyński <starczynski7@gmail.com>
2023-03-06 02:50:06 +0100

    add AccountTier, Tier FK to user, custom user forms for django-admin

---
+++ thumbify/apps/accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("email",)
